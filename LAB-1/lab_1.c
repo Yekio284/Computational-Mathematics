b9d6dfe3fbbd26edbeaa3830a3341871a2d4beb5
Yan Uryanskiy <71843750+Yekio284@users.noreply.github.com>
2022-03-20 17:20:33 +0700

    First lab work

--- LAB-1/lab_1.c
+++ LAB-1/lab_1.c
@@ -4,27 +4,21 @@
 
 #define SIZE 3
 
-void rsly_gauss(int n, double a[][n], double *x)
-{
+void gauss_method(int n, double a[][n], double *x) {
     int imax, i, j, k;
     double amax, c;
-    for (k = 0; k < n; k++)
-    {
+    for (k = 0; k < n; k++) {
         imax = k;
         amax = fabs(a[k][k]);
-        for (i = k + 1; i < n; i++)
-        {
-            if (fabs(a[i][k]) > amax)
-            {
+        for (i = k + 1; i < n; i++) {
+            if (fabs(a[i][k]) > amax) {
                 imax = i;
                 amax = fabs(a[j][k]);
             }
         }
 
-        if (k != imax) 
-        {
-            for (j = k; j < n; j++)
-            {
+        if (k != imax) {
+            for (j = k; j < n; j++) {
                 c = a[k][j];
                 a[k][j] = a[imax][j];
                 a[imax][j] = c;
@@ -39,16 +33,14 @@ void rsly_gauss(int n, double a[][n], double *x)
         for (i = k; i < n; i++)
 	        a[k][i] *= c;
         x[k] *= c;
-        for (i = k + 1; i < n; i++)
-        {
+        for (i = k + 1; i < n; i++) {
             for (j = k + 1; j < n; j++)
                 a[i][j] -= a[i][k]*a[k][j];
             x[i] -= a[i][k] * x[k];
         }
     }
 
-    for (i = n - 2; i >= 0; i--)
-    {
+    for (i = n - 2; i >= 0; i--) {
     	for (j = i + 1; j < n; j++)
     		x[i] -= a[i][j] * x[j];
     }
@@ -62,19 +54,16 @@ int main() {
     in = fopen("1.txt", "r");
     out = fopen("2.txt", "w");
 
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             fscanf(in, "%lf", &matrix[i][j]);
         }
 	fscanf(in, "%lf", &x[i]);
     }
 
-    rsly_gauss(SIZE, matrix, x);
+    gauss_method(SIZE, matrix, x);
     
-    for (int i = 0; i < SIZE; i++)
-    {
+    for (int i = 0; i < SIZE; i++) {
         fprintf(out, "x%d = %lf\n", i + 1, x[i]);
     }
     fclose(in);
