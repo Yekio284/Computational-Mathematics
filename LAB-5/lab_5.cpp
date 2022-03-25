00e918fa746d98dfe19f9f41b47987c6009bbdbb
Yan Uryanskiy <71843750+Yekio284@users.noreply.github.com>
2022-03-25 22:15:33 +0700

    Update lab_5.cpp

--- LAB-5/lab_5.cpp
+++ LAB-5/lab_5.cpp
@@ -22,11 +22,11 @@ double aitken_method(double *x, double *y, double inter, struct Length length) {
 
 int main() {
     FILE *in = fopen("in.txt", "r");
-	FILE *out = fopen("out.txt", "w");
+    FILE *out = fopen("out.txt", "w");
 	
-	double inter;
-	cout << "Введите число для интерполяции(x): ";
-	cin >> inter;
+    double inter;
+    cout << "Введите число для интерполяции(x): ";
+    cin >> inter;
 
     struct Length length;
     
@@ -34,46 +34,46 @@ int main() {
         length.y_length = length.y_length + length.x_length - i;
     }
 
-	double *x = new double[length.x_length];
-	double *y = new double[length.y_length];
+    double *x = new double[length.x_length];
+    double *y = new double[length.y_length];
 
-	for (int i = 0; i < length.x_length; i++) {
-		fscanf(in, "%lf", &x[i]);
+    for (int i = 0; i < length.x_length; i++) {
+        fscanf(in, "%lf", &x[i]);
+    }
+    for (int i = 0; i < length.x_length; i++) {
+	fscanf(in, "%lf", &y[i]);
+    }
+    
+    double result = aitken_method(x, y, inter, length);
+    cout << "Результат интерполяции: " << result << endl;
+
+    short a = 0, i = 0;
+    
+    while (i < length.x_length) {
+	if (inter > x[length.x_length - 1]) {
+	    for (int j = 0; j < length.x_length; j++) {
+                fprintf(out, "%lf %lf\n", x[j], y[j]);
+	    }
+	    fprintf(out, "%lf %lf\n", inter, result);
+            break;
 	}
-	for (int i = 0; i < length.x_length; i++) {
-		fscanf(in, "%lf", &y[i]);
+	else {
+	    if (x[i] < inter || a) {
+	        fprintf(out, "%lf %lf\n", x[i], y[i]);
+	        i++;
+	    }
+	    else if (!a) {
+	        fprintf(out, "%lf %lf\n", inter, result);
+		a = 1;
+	    }
 	}
+    }
 
-	double result = aitken_method(x, y, inter, length);
-	cout << "Результат интерполяции: " << result << endl;
-
-	short a = 0, i = 0;
-
-	while (i < length.x_length) {
-		if (inter > x[length.x_length - 1]) {
-			for (int j = 0; j < length.x_length; j++) {
-				fprintf(out, "%lf %lf\n", x[j], y[j]);
-			}
-			fprintf(out, "%lf %lf\n", inter, result);
-			break;
-		}
-		else {
-			if (x[i] < inter || a) {
-				fprintf(out, "%lf %lf\n", x[i], y[i]);
-				i++;
-			}
-			else if (!a) {
-				fprintf(out, "%lf %lf\n", inter, result);
-				a = 1;
-			}
-		}
-	}
-
-	fclose(in);
-	fclose(out);
-
-	delete[] x;
-	delete[] y;
+    fclose(in);
+    fclose(out);
+    
+    delete[] x;
+    delete[] y;
     
     return 0;
 }
