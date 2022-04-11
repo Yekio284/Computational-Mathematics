ad37bd4d09313c05cde17564f9772c8326e0c0f2
Yekio284 <yan.uryanskij@csc.sibsutis.ru>
2022-04-11 22:29:30 +0700

    Seventh lab work

--- LAB-7/lab_7.cpp
+++ LAB-7/lab_7.cpp
@@ -17,7 +17,39 @@ double right_rectangle_method(double a, double b, double n) {
     return s;
 }
 
+double trapeze_method(double a, double b, double n) {
+    double h = (b - a) / n;
+    double s = function(a) + function(b);
+
+    for (int i = 1; i <= n - 1; i++)
+        s = s + 2 * function(a + i * h);
+    
+    s = s * h / 2;
+    return s;
+}
+
+double simpson_method(double a, double b, double n) {
+    double h = (b - a) / n;
+    double s = function(a) + function(b);
+
+    for (int i = 1; i <= n - 1; i++) {
+        int k = 2 + 2 * (i % 2);
+        s = s + k * function(a + i * h);
+    }
+
+    s = s * h / 3;
+    return s;
+}
+
 int main() {
+    short choice;
+    cout << "Выберите метод интегрирования:\n1. Метод правых прямоугольников\n2. Метод трапеций\n3. Метод Симпсона\nВаш выбор: ";
+    cin >> choice;
+    if (choice != 1 && choice != 2 && choice != 3) {
+        cout << "Неверный ввод." << endl;
+        return -1;
+    }
+
     cout << "Введите отрезок [a; b]: ";
     double a, b;
     cin >> a >> b;
@@ -28,11 +60,27 @@ int main() {
 
     int k = 10; 
     int i = 1;
-    
-    while (abs(right_rectangle_method(a, b, k * i) - right_rectangle_method(a, b, k * (i + 1))) > epsilon)
-        i++;
-    
-    cout << "Результат интегрирования методом правых прямоугольников: S = " << right_rectangle_method(a, b, k * (i + 1)) << endl;
+
+    switch (choice) {
+    case 1:
+        while (abs(right_rectangle_method(a, b, k * i) - right_rectangle_method(a, b, k * (i + 1))) > epsilon)
+            i++;
+        cout << "Результат интегрирования методом правых прямоугольников: S = " << right_rectangle_method(a, b, k * (i + 1)) << endl;
+        break;
+    case 2:
+        while (abs(trapeze_method(a, b, k * i) - trapeze_method(a, b, k * (i + 1))) > epsilon)
+            i++;
+        cout << "Результат интегрирования методом трапеций: S = " << trapeze_method(a, b, k * (i + 1)) << endl;
+        break;
+    case 3:
+        while (abs(simpson_method(a, b, k * i) - simpson_method(a, b, k * (i + 1))) > epsilon)
+            i++;
+        cout << "Результат интегрирования методом Симпсона: S = " << simpson_method(a, b, k * (i + 1)) << endl;
+        break;
+
+    default:
+        break;
+    }   
 
     return 0;
 }
